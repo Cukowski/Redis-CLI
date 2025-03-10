cf9357e4c44853f43f04e790ed21ce51a8c58282
Cukowski <sel.ataaksoy@hotmail.com>
2025-03-10 16:01:22 +0100

    Part 5

---
+++ Part 5/Client/CLI.h
@@ -0,0 +1,19 @@
+#ifndef CLI_H
+#define CLI_H
+
+#include <string>
+//....
+#include "RedisClient.h"
+#include "CommandHandler.h"
+#include "ResponseParser.h"
+
+class CLI {
+public:
+    CLI(const std::string &host, int port);
+    void run();
+
+private:
+    RedisClient redisClient;
+};
+
+#endif // CLI_H
